7e4e88104b56cd7ddff2f6fae8e2b45bd56117d6
SaadEddinSalila <0959979@hr.nl>
2020-05-24 22:26:11 +0100

    start

---
+++ Models/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace meldboek.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [DisplayName("Uw email")]
+        [Required(ErrorMessage = "Deze veld is verplicht")]
+        public string Email { get; set; }
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Deze veld is verplicht")]
+        [DisplayName("Uw wachtwoord")]
+        public string Password { get; set; }
+        public string LoginErrorMessage { get; set; }
+
+
+        //constructor
+        public Person(int personId, string firstName, string lastName, string email, string password)
+        {
+            PersonId = personId;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+        }
+
+        //lege constructor
+        public Person()
+        {
+        }
+    }
+
+}
